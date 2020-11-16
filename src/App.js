e3a99b8a28d935edab646b840e1950b411e385a0
ellymaimon <eliranmaimon@gmail.com>
2020-11-16 08:19:19 -0800

    update App to save bitmoji url to db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useState } from 'react';
+import BitmojiButton from './BitmojiPicker';
+import faunadb from 'faunadb';
+
 function App() {
+  const [bitmojiURL, setBitmojiURL] = useState(null);
+
+  const saveToDB = () => {
+    if ( !bitmojiURL ) {
+      return;
+    }
+    const database = new faunadb.Client({ secret: process.env.REACT_APP_DB_KEY });
+    const q = faunadb.query;
+
+    database.query(
+      q.Create(
+        q.Collection('bitmojis'),
+        { data: { url: bitmojiURL } },
+      )
+    )
+  }
+
   return (
-    <div>
+    <div className='container'>
+      <BitmojiButton setBitmojiURL={setBitmojiURL} />
+      <button
+        onClick={saveToDB}
+        disabled={!bitmojiURL}
+      >
+        Save to DB
+      </button>
+      {bitmojiURL && <img src={bitmojiURL} alt='chosen bitmoji' /> }
     </div>
   );
 }
